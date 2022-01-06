f006452cf5f1323c10a1d58f3c3932933b8fe9f9
Akshay Bhatia <akshay.bhatia@infostride.com>
2022-01-06 21:21:36 +0530

    Added Main method which print 11-14 numbers

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+        public static void main(String[] args) {
+            int i=5;
+            int j=6;
+            int n=i+j;
+            do{ System.out.println(n);
+                n++;}
+            while(n<15);
+        }
+    }
+
