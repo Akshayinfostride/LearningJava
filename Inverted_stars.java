292f0b691c389b11a83fb5c0f0ec7e91ec33454a
Akshay Bhatia <akshay.bhatia@infostride.com>
2022-01-10 22:08:45 +0530

    Inverted Stars

---
+++ Inverted_stars.java
@@ -0,0 +1,13 @@
+public class Inverted_stars {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i <= 5; i++) {
+            for (j = i; j <= 5; j++) {
+                System.out.print("*");}
+            System.out.println(" ");
+
+        }
+    }
+}
+
+
